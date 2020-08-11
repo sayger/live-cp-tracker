911add5cb486bf6f1b32c63c4532057f142ec4e9
Aditya <icgadi99@gmail.com>
2020-08-11 15:05:49 +0530

    Setup fragment transition radio button

--- app/src/main/java/com/aditya/livecptracker/OngoingContestsFragment.kt
+++ app/src/main/java/com/aditya/livecptracker/OngoingContestsFragment.kt
@@ -6,12 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.aditya.livecptracker.databinding.FragmentOngoingContestsBinding
 
 class OngoingContestsFragment : Fragment() {
     private lateinit var binding : FragmentOngoingContestsBinding
+    private lateinit var navController: NavController
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_ongoing_contests, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding.radioButton.setOnCheckedChangeListener { group, checkedId ->
+            if(checkedId == R.id.upcoming_rb)
+                navController.navigate(R.id.action_ongoingContestsFragment_to_upcomingContestsFragment)
+        }
     }
 }
--- app/src/main/java/com/aditya/livecptracker/UpcomingContestsFragment.kt
+++ app/src/main/java/com/aditya/livecptracker/UpcomingContestsFragment.kt
@@ -6,13 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.aditya.livecptracker.databinding.FragmentUpcomingContestsBinding
 
 class UpcomingContestsFragment : Fragment() {
     private lateinit var binding : FragmentUpcomingContestsBinding
+    private lateinit var navController: NavController
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_upcoming_contests, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding.radioButton.setOnCheckedChangeListener{ group, checkedId ->
+            if(checkedId == R.id.ongoing_rb)
+                navController.navigate(R.id.action_upcomingContestsFragment_to_ongoingContestsFragment)
+        }
+    }
 }

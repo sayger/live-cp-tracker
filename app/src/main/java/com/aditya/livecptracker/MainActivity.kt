150af4620e751c8a3ca600e70f696f5b90f3a665
Aditya <icgadi99@gmail.com>
2020-08-12 10:07:10 +0530

    Added network status snackbar

--- app/src/main/java/com/aditya/livecptracker/MainActivity.kt
+++ app/src/main/java/com/aditya/livecptracker/MainActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.aditya.livecptracker.databinding.ActivityMainBinding
+import com.aditya.livecptracker.utils.isNetworkAvailable
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        if(!isNetworkAvailable(this)!!) {
+            Snackbar.make(binding.containerHead, "No Internet Connection! " +
+                    "Please check your network connection.", 5000).show()
+        }
     }
 }

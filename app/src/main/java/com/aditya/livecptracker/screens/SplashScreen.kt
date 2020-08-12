efd05880ffb2ad265250c2c5f7d7575a24a51229
Aditya <icgadi99@gmail.com>
2020-08-12 09:31:05 +0530

    Rearranged modules

--- app/src/main/java/com/aditya/livecptracker/screens/SplashScreen.kt
+++ app/src/main/java/com/aditya/livecptracker/screens/SplashScreen.kt
@@ -1,17 +1,21 @@
-package com.aditya.livecptracker
+package com.aditya.livecptracker.screens
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import androidx.databinding.DataBindingUtil
+import com.aditya.livecptracker.MainActivity
+import com.aditya.livecptracker.R
 import com.aditya.livecptracker.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
     private lateinit var binding : ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+        binding = DataBindingUtil.setContentView(this,
+            R.layout.activity_splash_screen
+        )
         Handler().postDelayed({
             val i = Intent(applicationContext, MainActivity::class.java)
             startActivity(i)
